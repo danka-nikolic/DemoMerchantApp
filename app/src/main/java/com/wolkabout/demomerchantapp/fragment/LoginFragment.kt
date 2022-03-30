0621b04b3618db11f1460590666e000df9311959
dnikolic <dankanikolic90@gmail.com>
2022-03-30 13:14:15 +0200

    Change loginButton action.

--- app/src/main/java/com/wolkabout/demomerchantapp/fragment/LoginFragment.kt
+++ app/src/main/java/com/wolkabout/demomerchantapp/fragment/LoginFragment.kt
@@ -1,12 +1,13 @@
 package com.wolkabout.demomerchantapp.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.wolkabout.demomerchantapp.databinding.FragmentLoginBinding
+import com.wolkabout.demomerchantapp.model.HomeActivity
 
 class LoginFragment: Fragment() {
 
@@ -27,8 +28,12 @@ class LoginFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.loginButton.setOnClickListener {
-            val actionMainFragmentToListFragment = LoginFragmentDirections.actionLoginFragmentToHomeFragment("rs")
-            findNavController().navigate(actionMainFragmentToListFragment)
+//            val actionMainFragmentToListFragment = LoginFragmentDirections.actionLoginFragmentToHomeFragment("rs")
+//            findNavController().navigate(actionMainFragmentToListFragment)
+           requireActivity().run {
+               startActivity(Intent(this, HomeActivity::class.java))
+               finish()
+           }
         }
     }
 
